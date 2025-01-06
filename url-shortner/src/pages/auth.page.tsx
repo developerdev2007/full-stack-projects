fb0b91f53b647ccf383286c86c2e88b9f8e4b042
developerdev2007 <developerdev2007@gmail.com>
2025-01-06 13:13:23 +0530

    auth page in working

--- url-shortner/src/pages/auth.page.tsx
+++ url-shortner/src/pages/auth.page.tsx
@@ -1,4 +1,16 @@
+import { useSearchParams } from "react-router-dom";
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [search] = useSearchParams();
+  return (
+    <div className="mt-36 flex flex-1 flex-col items-center gap-10 ">
+      {" "}
+      <h1 className="text-5xl font-semibold  ">
+        {search.get("createNew")
+          ? "Hold Up lets login first..."
+          : "Login / SignUp"}
+      </h1>
+    </div>
+  );
 };
 export default AuthPage;
